c6f5c85be606ae01099818c378858330e01724fa
BuketBeker <buketbeker@gmail.com>
2022-09-19 17:34:07 +0300

    first commit

---
+++ src/context/reducer.js
@@ -0,0 +1,49 @@
+export const initialState = {
+    todos: [],
+};
+const reducer = (state, action) => {
+    console.log(action) //for debug
+    switch(action.type){
+        case "ADD_TODO":
+            return {
+                ...state,
+                todos: [action.payload, ...state.todos],
+            };
+        case "REMOVE_TODO":
+            return {
+                ...state,
+                todos: [...state.todos].filter((todo) => todo.id !== action.payload),
+            };
+        case "UPDATE_TODO":
+            return {
+                ...state,
+                todos: state.todos.map((todo) => {
+                    if(todo.id !== action.payload.todoId){
+                        return todo;
+                    }
+                    return {
+                        ...todo,
+                        content: action.payload.newValue,
+                    };
+                }),
+            };
+        case "COMPLETE_TODO":
+            return {
+                ...state,
+                todos: state.todos.map((todo) => {
+                    if(todo.id !== action.payload){
+                        return todo;
+                    }
+                    return {
+                        ...todo,
+                        isCompleted: !todo.isCompleted,
+                    };
+                }),
+            };
+        default:
+            return {
+                ...state,
+            };
+    }
+};
+export default reducer;
