b0c8e7cf43b40de03a0b390aad88c54f62c5ae8f
Buket Beker <66417027+BuketBeker@users.noreply.github.com>
2022-10-05 13:28:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef, } from 'react';
+import { useTodoLayerValue } from './context/TodoContext';
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [{todos}, dispatch] = useTodoLayerValue();
+  const [content, setContent] = useState('');
+
+  const inputRef = useRef(null);
+
+  useEffect (() => {
+    inputRef.current.focus(); 
+  },[]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(content){
+      const newTodo = {
+        id: Math.floor(Math.random() * 39399393),
+        content,
+        isCompeted: false,
+      };
+
+      dispatch({
+        type: 'ADD_TODO', 
+        payload: newTodo,
+      });
+      setContent('');
+    }
+  };
+
+  return(
+    <div className='container'>
+      <form onSubmit={handleSubmit} className='todo-form'>
+        <input type='text' className='todo-input'
+        onChange = {(event) => setContent(event.target.value)}
+        value={content} ref={inputRef}/>
+        <button className='todo-button'>Ekle</button>
+      </form>
+      <TodoList todos={todos}/>
     </div>
   );
-}
+};
 
 export default App;
